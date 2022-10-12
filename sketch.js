99ac959a5e4938914f55e762b6cec6f2da304411
Lívia Luchiari <liviaangelice@gmail.com>
2022-10-12 06:54:25 -0300

    Mudei as imagens de assets p/ pasta geral
    Tirei a imagem de car1, car2 e pista de assets e coloquei na pasta geral pra testar.

--- sketch.js
+++ sketch.js
@@ -10,9 +10,9 @@ var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/PISTA.png");
+  car1_img = loadImage("./car1.png");
+  car2_img = loadImage("./car2.png");
+  track = loadImage("./PISTA.png");
   fuelImage = loadImage("./assets/fuel.png");
   powerCoinImage = loadImage("./assets/goldCoin.png");
   obstacle1Image = loadImage("./assets/obstacle1.png");
